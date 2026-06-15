a6386b2a96675ac3f05c11c6e47a504e2af5da2e
Beru <lidor.y.shapira@gmail.com>
2026-06-15 18:09:41 +0300

    fixed file logger naming

--- Assets/Scripts/Adapter/FileLogger.cs
+++ Assets/Scripts/Adapter/FileLogger.cs
@@ -4,8 +4,8 @@ namespace Patterns.Structural.Adapter
 {
     public class FileLogger
     {
-        static string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-        private readonly string _filePath =  Path.Combine(desktopPath, "myLogger.txt");
+        private static string _desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        private readonly string _filePath =  Path.Combine(_desktopPath, "myLogger.txt");
         public void WriteLine(string text, int level)
         {
             using (StreamWriter sw = new StreamWriter(_filePath, true))
